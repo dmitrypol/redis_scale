b32c58fa8ba2e4cf53397b2476f51fad588c2a6f
Dmitry <dmitrypol@gmail.com>
2019-03-26 05:59:45 -0700

    added hash_tag to envoy worker

--- envoy/worker/worker.py
+++ envoy/worker/worker.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     r = redis.Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'])
     pipe = r.pipeline(transaction=False)
     while True:
-        unique_user_id = str(uuid.uuid4())
+        hash_tag = '{}'
+        unique_user_id = hash_tag + str(uuid.uuid4())
         coordinates = {'lon':uniform(-180,180), 'lat':uniform(-90,90)}
         pipe.hmset(unique_user_id, coordinates)
         pipe.expire(unique_user_id, 60)
