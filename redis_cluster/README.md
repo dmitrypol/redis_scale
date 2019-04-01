76377bbaea2ef810a8a6d65dd283f5bdca95e0e3
Dmitry <dmitrypol@gmail.com>
2019-04-01 10:29:34 -0700

    added 2 more Redis nodes to cluster

--- redis_cluster/README.md
+++ redis_cluster/README.md
@@ -1,15 +1,25 @@
+rm -f 700*/nodes.conf 700*/dump.rdb 700*/appendonly.aof
+
+pkill -f redis-server
+
 honcho start
 
 redis-cli --cluster create 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005 --cluster-replicas 1
 
-./web.py
-
-http://localhost:5000/
-
-./worker.py
-
 redis-cli -p 7000 cluster info
 
-rm -f 700*/nodes.conf 700*/dump.rdb 700*/appendonly.aof
+redis-cli -p 7000 cluster nodes
 
-pkill -f redis-server
+redis-cli -p 7000 cluster failover
+
+redis-cli -p 7000 debug segfault
+
+redis-cli --cluster check 127.0.0.1:7000
+
+redis-cli --cluster add-node 127.0.0.1:7006 127.0.0.1:7000
+
+redis-cli --cluster add-node 127.0.0.1:7007 127.0.0.1:7000 --cluster-slave --cluster-master-id node-id
+
+redis-cli --cluster reshard 127.0.0.1:7000
+
+redis-cli --cluster del-node 127.0.0.1:7000 node-id
--- redis_cluster/web.py
+++ redis_cluster/web.py
@@ -7,24 +7,27 @@ from rediscluster import StrictRedisCluster
 
 app = Flask(__name__)
 
-startup_nodes = [{'host': 'localhost', 'port': '7000'}]
-r = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+
+def redis_client():
+    startup_nodes = [{'host': 'localhost', 'port': '7000'}]
+    r = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+    return(r)
 
 
 @app.route("/")
 def web(dbsize=None, count=None, total=None, used_memory=None):
 
     start_time = 0
-    if r.get('start_time'): start_time = r.get('start_time')
+    if redis_client().get('start_time'): start_time = redis_client().get('start_time')
     run_time = int(time.time() - float(start_time))
 
-    info_memory = r.info(section='memory')
+    info_memory = redis_client().info(section='memory')
     tmp_memory = 0
     for key, value in info_memory.items():
         tmp_memory += int(value['used_memory'])
     used_memory = ("{:,}".format(tmp_memory))
 
-    dbsize = r.dbsize()
+    dbsize = redis_client().dbsize()
     tmp_total=sum(dbsize.values())
     num_keys = ("{:,}".format(tmp_total))
 
@@ -43,17 +46,17 @@ def web(dbsize=None, count=None, total=None, used_memory=None):
 
 @app.route("/info")
 def info():
-    return(str(r.cluster('info')))
+    return(str(redis_client().cluster('info')))
 
 
 @app.route("/nodes")
 def nodes():
-    return(str(r.cluster('nodes')))
+    return(str(redis_client().cluster('nodes')))
 
 
 @app.route("/slots")
 def slots():
-    return(str(r.cluster('slots')))
+    return(str(redis_client().cluster('slots')))
 
 
 if __name__ == '__main__':
